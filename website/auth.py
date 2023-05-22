e974c9a8d2013c83d355ddce540ecc903155cf6d
millamy <gutovskanina@gmail.com>
2023-05-22 14:06:43 +0200

    fix minor errors

--- website/auth.py
+++ website/auth.py
@@ -19,7 +19,7 @@ def login():
             if check_password_hash(user.password,password):
                 flash('Logged in successfully!',category = 'success')
                 login_user(user,remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.index'))
             else:
                 flash('Incorrect password!',category = 'error')
         else:
