dd2358a2147ae291b542c27b8f029f5721bb30d1
millamy <gutovskanina@gmail.com>
2023-06-14 09:33:40 +0200

    add calc avg functions

--- website/functions.py
+++ website/functions.py
@@ -1,5 +1,5 @@
 from . import db
-from .models import University,Course
+from .models import University,Course,Rating
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -80,3 +80,37 @@ def send_email(sender_email, sender_password, recipient_email, subject, message)
         server.quit()
 
 
+def calculate_average_quality_value(course):
+    ratings = course.rating  # Assuming 'rating' is the relationship between Course and Rating
+    total_ratings = len(ratings)
+
+    if total_ratings == 0:
+        return 0
+
+    sum_quality_values = sum(rating.quality_value for rating in ratings)
+    average_quality_value = sum_quality_values / total_ratings
+
+    return average_quality_value
+
+
+def calculate_average_difficulty_value(course):
+    ratings = course.rating  # Assuming 'rating' is the relationship between Course and Rating
+    total_ratings = len(ratings)
+
+    if total_ratings == 0:
+        return 0
+
+    sum_difficulty_values = sum(rating.difficulty_value for rating in ratings)
+    average_difficulty_value = sum_difficulty_values / total_ratings
+
+    return average_difficulty_value
+
+def calculate_average_rating(course):
+    ratings = Rating.query.filter_by(course_id=course.course_id).all()
+    if ratings:
+        total_ratings = len(ratings)
+        quality_sum = sum(rating.quality_value for rating in ratings)
+        average_rating = quality_sum / total_ratings
+        return round(average_rating,2)
+    else:
+        return 'Brak opinii'
